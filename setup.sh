b4af180db7e8532fd539972e67bc0375fb7319d9
Miguel Berrio <mail@berrio.dev>
2024-03-10 21:08:14 -0500

    feat(setup): add script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+LOCAL_BIN=$HOME/.local/bin
+export PATH=$PATH:$LOCAL_BIN
+
+mkdir -p "$LOCAL_BIN"
+
+which chezmoi >/dev/null 2>&1 || sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
+
+chezmoi init --apply https://github.com/B3RR10/dotfiles.git
