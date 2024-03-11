14ce2b44ba79ce2189051686ca84e0ca83a1ef20
Miguel Berrio <mail@berrio.dev>
2024-03-11 07:58:05 -0500

    feat(setup): only init chezmoi when it does not exist

--- setup.sh
+++ setup.sh
@@ -9,4 +9,8 @@ mkdir -p "$LOCAL_BIN"
 
 which chezmoi >/dev/null 2>&1 || sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 
-chezmoi init --apply https://github.com/B3RR10/dotfiles.git
+if [ ! -d "$HOME/.local/share/chezmoi" ]; then
+	chezmoi init --apply https://github.com/B3RR10/dotfiles.git --promptDefaults
+else
+	chezmoi update --force
+fi
