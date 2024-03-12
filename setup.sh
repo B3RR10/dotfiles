55b9a66d9364529066dd2100ae4c92537c98e2f0
Miguel Berrio <mail@berrio.dev>
2024-03-12 13:30:49 -0500

    feat(setup): update chezmoi config before update

--- setup.sh
+++ setup.sh
@@ -14,5 +14,6 @@ which chezmoi >/dev/null 2>&1 || sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b
 if [ ! -d "$HOME/.local/share/chezmoi" ]; then
 	chezmoi init --apply https://github.com/B3RR10/dotfiles.git --promptDefaults
 else
+	chezmoi init --promptDefaults
 	chezmoi update --force
 fi
