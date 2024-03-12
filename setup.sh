5bc926b139e631384325c1bd0cedc943de9f4c4b
Miguel Berrio <mail@berrio.dev>
2024-03-12 08:39:01 -0500

    feat(setup): create /usr/local/bin if not existing

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ export PATH=$PATH:$LOCAL_BIN
 
 mkdir -p "$LOCAL_BIN"
 
+[ -d /usr/local/bin ] || sudo mkdir -p /usr/local/bin
+
 which chezmoi >/dev/null 2>&1 || sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 
 if [ ! -d "$HOME/.local/share/chezmoi" ]; then
