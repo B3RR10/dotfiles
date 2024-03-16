9c29d1334c924c7559169e677a658a7243735142
Miguel Berrio <mail@berrio.dev>
2024-03-15 21:02:04 -0500

    feat(nvim): remove window.nvim

--- dot_config/nvim/lua/base/editor.lua
+++ dot_config/nvim/lua/base/editor.lua
@@ -260,26 +260,6 @@ return {
     event = 'VeryLazy',
   },
   {
-    'anuvyklack/windows.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'anuvyklack/middleclass',
-      'anuvyklack/animation.nvim',
-    },
-    opts = {
-      ignore = {
-        buftype = { 'quickfix' },
-        filetype = { 'NvimTree' },
-      },
-    },
-    config = function(_, opts)
-      vim.o.winwidth = 10
-      vim.o.winminwidth = 10
-      vim.o.equalalways = false
-      require('windows').setup(opts)
-    end,
-  },
-  {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = {
