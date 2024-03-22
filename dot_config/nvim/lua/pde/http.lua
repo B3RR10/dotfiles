f079fad7bd45cba145c007e7d5174ea435c34c58
Miguel Berrio <mail@berrio.dev>
2024-03-22 09:01:53 -0500

    feat(nvim): remove rest.nvim

--- dot_config/nvim/lua/pde/http.lua
+++ dot_config/nvim/lua/pde/http.lua
@@ -10,25 +10,41 @@ return {
       end
     end,
   },
-  {
-    'rest-nvim/rest.nvim',
-    dependencies = { 'plenary.nvim' },
-    ft = 'http',
-    opts = {
-      result_split_horizontal = true,
-      skip_ssl_verification = false,
-      highlight = {
-        enabled = false,
-        timeout = 150,
-      },
-      jump_to_request = false,
-    },
-    config = function(opts)
-      require('rest-nvim').setup(opts)
-
-      vim.keymap.set('n', '<Leader>x', '<Plug>RestNvim', { desc = 'Execute request' })
-    end,
-  },
+  -- {
+  --   'rest-nvim/rest.nvim',
+  --   dependencies = {
+  --     'plenary.nvim',
+  --     {
+  --       'vhyrro/luarocks.nvim',
+  --       -- config = true,
+  --       opts = {
+  --         rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
+  --       },
+  --     },
+  --   },
+  --   -- cmd = 'Rest',
+  --   ft = 'http',
+  --   opts = {
+  --     skip_ssl_verification = false,
+  --     result = {
+  --       split = {
+  --         horizontal = true,
+  --         stay_in_current_window_after_split = true,
+  --       },
+  --     },
+  --     highlight = {
+  --       enable = false,
+  --       timeout = 150,
+  --     },
+  --     -- keybinds = {
+  --     --   { '<Leader>rr', '<wmd>Rest run<CR>', 'Run request under cursor' },
+  --     --   { '<Leader>rl', '<wmd>Rest run last<CR>', 'Re-run last request' },
+  --     -- },
+  --   },
+  --   config = function(_, opts)
+  --     require('rest-nvim').setup(opts)
+  --   end,
+  -- },
   {
     'mattn/emmet-vim',
     ft = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'vue' },
