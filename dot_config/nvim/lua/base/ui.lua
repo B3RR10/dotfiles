4066cf89ced83975704afca1530552b3eb686b87
Miguel Berrio <mail@berrio.dev>
2024-06-03 07:50:35 -0500

    feat(nvim): remove material icons

--- dot_config/nvim/lua/base/ui.lua
+++ dot_config/nvim/lua/base/ui.lua
@@ -132,13 +132,5 @@ return {
       },
     },
   },
-  {
-    'nvim-tree/nvim-web-devicons',
-    dependencies = { 'DaikyXendo/nvim-material-icon' },
-    config = function()
-      require('nvim-web-devicons').setup({
-        override = require('nvim-material-icon').get_icons(),
-      })
-    end,
-  },
+  { 'nvim-tree/nvim-web-devicons' },
 }
