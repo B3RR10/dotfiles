3eaf015f6307fb89d56759817163b4be45de406f
Miguel Berrio <mail@berrio.dev>
2024-08-29 13:05:13 -0500

    feat(nvim): load go plugs only on go files

--- dot_config/nvim/lua/pde/go.lua
+++ dot_config/nvim/lua/pde/go.lua
@@ -63,7 +63,6 @@ return {
     config = function(_, opts)
       require('go').setup(opts)
     end,
-    event = { 'CmdlineEnter' },
     ft = { 'go', 'gomod' },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
