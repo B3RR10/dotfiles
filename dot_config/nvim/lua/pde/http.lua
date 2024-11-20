2580014130a2f679cc034e0bf35ae8a0a483bc92
Miguel Berrio <mail@berrio.dev>
2024-11-20 09:37:54 -0500

    feat(nvim): enable rest.nvim

--- dot_config/nvim/lua/pde/http.lua
+++ dot_config/nvim/lua/pde/http.lua
@@ -10,41 +10,10 @@ return {
       end
     end,
   },
-  -- {
-  --   'rest-nvim/rest.nvim',
-  --   dependencies = {
-  --     'plenary.nvim',
-  --     {
-  --       'vhyrro/luarocks.nvim',
-  --       -- config = true,
-  --       opts = {
-  --         rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
-  --       },
-  --     },
-  --   },
-  --   -- cmd = 'Rest',
-  --   ft = 'http',
-  --   opts = {
-  --     skip_ssl_verification = false,
-  --     result = {
-  --       split = {
-  --         horizontal = true,
-  --         stay_in_current_window_after_split = true,
-  --       },
-  --     },
-  --     highlight = {
-  --       enable = false,
-  --       timeout = 150,
-  --     },
-  --     -- keybinds = {
-  --     --   { '<Leader>rr', '<wmd>Rest run<CR>', 'Run request under cursor' },
-  --     --   { '<Leader>rl', '<wmd>Rest run last<CR>', 'Re-run last request' },
-  --     -- },
-  --   },
-  --   config = function(_, opts)
-  --     require('rest-nvim').setup(opts)
-  --   end,
-  -- },
+  {
+    'rest-nvim/rest.nvim',
+    ft = 'http',
+  },
   {
     'mattn/emmet-vim',
     ft = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'vue' },
