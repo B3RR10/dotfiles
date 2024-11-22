94938fb8cd1b20ec813ce0ed7323a04358d3462a
Miguel Berrio <mail@berrio.dev>
2024-11-22 16:18:19 -0500

    feat(nvim): update F# lsp config

--- dot_config/nvim/lua/pde/fsharp.lua
+++ dot_config/nvim/lua/pde/fsharp.lua
@@ -4,6 +4,12 @@ end
 
 return {
   {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { 'fsharp' })
+    end,
+  },
+  {
     'williamboman/mason.nvim',
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
@@ -14,17 +20,11 @@ return {
     end,
   },
   {
-    'neovim/nvim-lspconfig',
-    opts = {
-      servers = {
-        fsautocomplete = {
-          cmd = { 'fsautocomplete', '--background-service-enabled', '--verbose' },
-          root_dir = require('lspconfig.util').root_pattern('*.sln', '.git', '*.fsproj', '*.fsx'),
-        },
-      },
-    },
+    'ionide/Ionide-vim',
+    ft = { 'fsharp' },
   },
   {
     'adelarsq/neofsharp.vim',
+    ft = { 'fsharp' },
   },
 }
