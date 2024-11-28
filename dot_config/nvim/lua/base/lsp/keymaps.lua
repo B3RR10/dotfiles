338a7e6a1d941399bc1768c557192d90275ca454
Miguel Berrio <mail@berrio.dev>
2024-11-28 12:36:38 -0500

    refactor(nvim): format lsp keymaps

--- dot_config/nvim/lua/base/lsp/keymaps.lua
+++ dot_config/nvim/lua/base/lsp/keymaps.lua
@@ -5,36 +5,35 @@ function M.on_attach(client, buffer)
 
   local telescope = require('telescope.builtin')
 
-  -- stylua: ignore
-  self:map('gd', function() telescope.lsp_definitions({ reuse_win = true }) end, { desc = 'Goto Definition' })
-  self:map('gr', telescope.lsp_references, { desc = 'References' })
-  -- stylua: ignore
-  self:map('gI', function() telescope.lsp_implementations({ reuse_win = true }) end, { desc = 'Goto Implementation' })
-  -- stylua: ignore
+  -- stylua: ignore start
+  self:map('gd', function() telescope.lsp_definitions({ reuse_win = true }) end,      { desc = 'Goto Definition' })
+  self:map('gr', telescope.lsp_references,                                            { desc = 'References' })
+  self:map('gI', function() telescope.lsp_implementations({ reuse_win = true }) end,  { desc = 'Goto Implementation' })
   self:map('gy', function() telescope.lsp_type_definitions({ reuse_win = true }) end, { desc = 'Goto Type Definition' })
 
-  self:map('K', vim.lsp.buf.hover, { desc = 'Hover' })
+  self:map('K',  vim.lsp.buf.hover,          { desc = 'Hover' })
   self:map('gK', vim.lsp.buf.signature_help, { desc = 'Signature Help', has = 'signatureHelp' })
 
-  -- stylua: ignore
   self:map('<leader>ld', function() telescope.diagnostics({ bufnr = 0 }) end, { desc = 'Show document diagnostics' })
-  self:map('<leader>lD', telescope.diagnostics, { desc = 'Show workspace diagnostics' })
-  self:map('[d', M.diagnostic_goto(false), { desc = 'Prev Diagnostic' })
-  self:map(']d', M.diagnostic_goto(true), { desc = 'Next Diagnostic' })
-  self:map('[e', M.diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
-  self:map(']e', M.diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
+  self:map('<leader>lD', telescope.diagnostics,                               { desc = 'Show workspace diagnostics' })
+  self:map('[d',         M.diagnostic_goto(false),                            { desc = 'Prev Diagnostic' })
+  self:map(']d',         M.diagnostic_goto(true),                             { desc = 'Next Diagnostic' })
+
+  self:map('[e', M.diagnostic_goto(false, 'ERROR'),   { desc = 'Prev Error' })
+  self:map(']e', M.diagnostic_goto(true,  'ERROR'),   { desc = 'Next Error' })
   self:map('[w', M.diagnostic_goto(false, 'WARNING'), { desc = 'Prev Warning' })
-  self:map(']w', M.diagnostic_goto(true, 'WARNING'), { desc = 'Next Warning' })
+  self:map(']w', M.diagnostic_goto(true,  'WARNING'), { desc = 'Next Warning' })
 
   self:map('<leader>la', vim.lsp.buf.code_action, { desc = 'Code Action', mode = { 'n', 'v' }, has = 'codeAction' })
 
   local format = require('base.lsp.format').format
-  self:map('<leader>lf', format, { desc = 'Format Document', has = 'documentFormatting' })
-  self:map('<leader>lf', format, { desc = 'Format Range', mode = 'v', has = 'documentRangeFormatting' })
-  self:map('<leader>lr', vim.lsp.buf.rename, { expr = true, desc = 'Rename', has = 'rename' })
+  self:map('<leader>lf', format,             { desc = 'Format Document', has = 'documentFormatting' })
+  self:map('<leader>lf', format,             { desc = 'Format Range',    mode = 'v',      has = 'documentRangeFormatting' })
+  self:map('<leader>lr', vim.lsp.buf.rename, { expr = true,              desc = 'Rename', has = 'rename' })
 
-  self:map('<leader>ls', telescope.lsp_document_symbols, { desc = 'Document Symbols' })
+  self:map('<leader>ls', telescope.lsp_document_symbols,          { desc = 'Document Symbols' })
   self:map('<leader>lS', telescope.lsp_dynamic_workspace_symbols, { desc = 'Workspace Symbols' })
+  -- stylua: ignore end
 end
 
 function M.new(client, buffer)
