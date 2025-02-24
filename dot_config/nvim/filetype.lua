ac7a5945b69974e82f7913d627eb8037b96c2e25
Miguel Berrio <mail@berrio.dev>
2025-02-24 11:25:26 -0500

    feat(nvim): remove plugin nvim-ansible in favor of manual filetype detection

--- dot_config/nvim/filetype.lua
+++ dot_config/nvim/filetype.lua
@@ -1,11 +1,26 @@
 vim.filetype.add({
   extension = {
+    -- helm
     gotmpl = 'helm',
   },
   pattern = {
+    -- Ansible
+    ['.*/defaults/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/host_vars/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/group_vars/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/group_vars/.*/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/playbook.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/playbooks/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/roles/.*/tasks/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/roles/.*/handlers/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/tasks/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/molecule/.*%.ya?ml'] = 'yaml.ansible',
+
+    -- docker-compose
     ['compose.*%.ya?ml'] = 'yaml.docker-compose',
     ['docker%-compose.*%.ya?ml'] = 'yaml.docker-compose',
 
+    -- helm
     ['.*/templates/.*%.ya?ml'] = 'helm',
     ['.*/templates/.*%.tpl'] = 'helm',
     ['.*/templates/.*%.txt'] = 'helm',
