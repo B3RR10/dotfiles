c186a98853f9c0daec281402d62361efc0bb5638
Miguel Berrio <mail@berrio.dev>
2025-03-07 05:58:55 -0500

    feat(nvim): add harpoon symbol to lualine, remove session

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -5,12 +5,30 @@ return {
   },
   event = 'UIEnter',
   opts = function()
-    local function isSession()
-      if vim.v.this_session ~= '' then
-        return '▣ '
-      else
-        return ''
+    local function harpooned()
+      local harpoon = require('harpoon')
+      local harpoon_icon = require('mini.icons').get('filetype', 'harpoon')
+
+      local root_dir = harpoon:list().config.get_root_dir()
+      local current_file_path = vim.api.nvim_buf_get_name(0)
+
+      local harpoon_items = harpoon:list().items
+
+      for _, harpoon_item in ipairs(harpoon_items) do
+        local harpoon_path = harpoon_item.value
+
+        -- If relative path, prepend root_dir
+        if string.sub(harpoon_path, 1, 1) ~= '/' then
+          local path_separator = '/'
+          if vim.fn.has('win32') == 1 or vim.fn.has('win32unix') == 1 then path_separator = '\\' end
+
+          harpoon_path = root_dir .. path_separator .. harpoon_path
+        end
+
+        if harpoon_path == current_file_path then return harpoon_icon end
       end
+
+      return ''
     end
 
     --- @param trunc_width number trunctates component when screen width is less than trunc_width
@@ -41,9 +59,9 @@ return {
         always_divide_middle = true,
       },
       sections = {
-        lualine_a = { 'mode', isSession },
+        lualine_a = { 'mode', harpooned },
         lualine_b = {
-          { 'branch', icon = '', fmt = trunc(1000, 20, 80, false) },
+          { 'FugitiveHead', icon = '', fmt = trunc(1000, 20, 80, false) },
           { 'diff' }, --, source = diff_sources },
           { 'diagnostics', sources = { 'nvim_lsp' } },
         },
@@ -52,7 +70,11 @@ return {
         },
         lualine_x = {
           'copilot',
-          { require('lazy.status').updates, cond = require('lazy.status').has_updates, color = { fg = 'ff9e64' } },
+          {
+            require('lazy.status').updates,
+            cond = require('lazy.status').has_updates,
+            color = { fg = 'ff9e64' },
+          },
           'searchcount',
           { 'encoding', show_bomb = true },
           'fileformat',
