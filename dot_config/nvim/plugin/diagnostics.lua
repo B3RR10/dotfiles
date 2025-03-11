a48dbd6279fd071b23c4f0797fca71cfd0a98caa
Miguel Berrio <mail@berrio.dev>
2025-03-11 14:53:18 -0500

    feat(nvim): update diagnostics config

--- dot_config/nvim/plugin/diagnostics.lua
+++ dot_config/nvim/plugin/diagnostics.lua
@@ -1,6 +1,20 @@
 -- Diagnostic configuration
 vim.diagnostic.config({
   severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+    },
+  },
   float = {
     border = 'rounded',
     source = 'if_many',
@@ -15,6 +29,7 @@ vim.lsp.handlers['textDocument/hover'] =
 vim.lsp.handlers['textDocument/signatureHelp'] =
   vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
 
+-- TODO: what for?
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event) vim.diagnostic.enable(true, { bufnr = event.buf }) end,
 })
