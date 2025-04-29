849d531034d7e0682d1bea2055cfcbd8c3084ec8
Miguel Berrio <mail@berrio.dev>
2025-04-29 09:18:03 -0500

    feat(nvim): update keymaps

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -5,13 +5,12 @@ vim.keymap.set('i', 'JK', '<ESC>', { noremap = true })
 -- Moving chars
 vim.keymap.set('n', 'L', 'xp', { noremap = true })
 vim.keymap.set('n', 'H', 'Xph', { noremap = true })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- Keep visual selection after indent
 vim.keymap.set('v', '<', '<gv', { noremap = true })
 vim.keymap.set('v', '>', '>gv', { noremap = true })
-
--- Copy to end of line
-vim.keymap.set('', 'Y', 'y$', { noremap = true })
 
 -- nohlsearch
 vim.keymap.set('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
