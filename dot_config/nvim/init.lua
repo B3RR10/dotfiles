49ddfa6df40527eedd43a2386ef63a62657f9a88
Miguel Berrio <mail@berrio.dev>
2025-05-05 09:43:04 -0500

    feat(nvim): update blink.cmp to v1.2.0

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -80,7 +80,8 @@ later({
   add = {
     source = 'saghen/blink.cmp',
     depends = { 'rafamadriz/friendly-snippets' },
-    checkout = 'v1.1.1',
+    checkout = 'v1.2.0',
+    monitor = 'main',
     hooks = {
       post_checkout = function() vim.system({ 'cargo', 'build', '--release' }):wait() end,
       post_install = function() vim.system({ 'cargo', 'build', '--release' }):wait() end,
