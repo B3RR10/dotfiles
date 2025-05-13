29016c799b73660d588e90230e2e3d52fe75ed4f
Miguel Berrio <mail@berrio.dev>
2025-05-13 07:17:09 -0500

    feat(nvim): update ionide config

--- dot_config/nvim/lua/plugins/ionide.lua
+++ dot_config/nvim/lua/plugins/ionide.lua
@@ -1,9 +1,10 @@
-local capabilities = require('blink.cmp').get_lsp_capabilities()
+vim.g['fsharp#lsp_auto_setup'] = 0
+vim.g['fsharp#exclude_project_directories'] = { 'paket-files' }
 
 require('ionide').setup({
   autostart = true,
   flags = {
     debounce_text_changes = 150,
   },
-  capabilities = capabilities,
+  capabilities = require('blink.cmp').get_lsp_capabilities(),
 })
