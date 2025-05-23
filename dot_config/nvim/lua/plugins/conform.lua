90b908ab8d0f426e62c6df71a92768fb1ddefe61
Miguel Berrio <mail@berrio.dev>
2025-05-23 14:30:29 -0500

    feat(nvim): allow disabling autoformat

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -20,7 +20,12 @@ conform.setup({
   default_format_opts = {
     lsp_format = 'fallback',
   },
-  format_on_save = { timeout_ms = 500 },
+  format_on_save = function(bufnr)
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+    return {
+      timeout_ms = 500,
+    }
+  end,
   formatters = {
     shfmt = {
       prepend_args = { '-i', '2' },
@@ -30,9 +35,48 @@ conform.setup({
 
 vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- Disable autoformat for this buffer only
+    vim.b.disable_autoformat = true
+    vim.notify('Disabled autoformat for current buffer')
+  else
+    -- Disable autoformat globally
+    vim.g.disable_autoformat = true
+    vim.notify('Disabled autoformat globally', vim.log.levels.WARN)
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+  vim.notify('Enabled autoformat')
+end, {
+  desc = 'Enable autoformat-on-save',
+})
+
 vim.keymap.set(
   { 'n', 'v' },
   'grf',
   function() conform.format({ async = true }) end,
   { desc = 'Format buffer' }
 )
+
+vim.keymap.set('n', '<leader>tf', function()
+  if vim.b.disable_autoformat then
+    vim.cmd.FormatEnable()
+  else
+    vim.cmd.FormatDisable({ bang = true })
+  end
+end, { desc = 'Toggle autoformat for current buffer' })
+
+vim.keymap.set('n', '<leader>tF', function()
+  if vim.b.disable_autoformat then
+    vim.cmd.FormatEnable()
+  else
+    vim.cmd.FormatDisable()
+  end
+end, { desc = 'Toggle autoformat for current buffer' })
